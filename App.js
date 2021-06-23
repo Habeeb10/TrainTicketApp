b8d2d8b713bdc8f7e321663702f2f4764c06dd32
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-06-23 18:02:26 +0100

    feat:added tab navigation

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+// import React from "react";
+// import { StyleSheet, Text, View } from "react-native";
+// import Home from "./src/screens/home/Home";
+
+// export default function App() {
+//   return <Home />;
+// }
+import "react-native-gesture-handler";
+import React from "react";
+
+import Navigator from "./src/Navigator/Navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
