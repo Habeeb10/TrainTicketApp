f89ab9d5aa2d053990abc2586722db57ee21dc57
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-06-23 18:05:25 +0100

    feat: created Header reusable component

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export const Header = ({ icon, title, onpress, headerStyle }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onpress={onpress}>{icon}</TouchableOpacity>
+      <Text style={[styles.title, headerStyle]}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    marginTop: 70,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  title: {
+    color: "#333E63",
+    fontSize: 18,
+    fontWeight: "700",
+    lineHeight: 33,
+    alignSelf: "center",
+  },
+});
